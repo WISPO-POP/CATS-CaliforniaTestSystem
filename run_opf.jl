b88aa22140f46a04bad0db8ab4aeb1b70e07e796
Sofia Taylor <71098965+Sofia-Taylor@users.noreply.github.com>
2022-09-01 12:30:51 -0600

    Add run_opf file

---
+++ run_opf.jl
@@ -0,0 +1,48 @@
+#########################################################################################################
+# run_opf.jl
+#
+# Run a DC power flow
+#
+#########################################################################################################
+
+###########
+## Setup ##
+###########
+
+# Load Julia Packages
+#--------------------
+using PowerModels
+using JuMP
+using CSV, JSON
+using DataFrames
+using Ipopt
+
+# User input
+#-----------
+save_to_JSON = false
+
+# Specify solver
+#----------------
+# const IPOPT_ENV = Ipopt.Env()
+solver = Ipopt.Optimizer # JuMP.optimizer_with_attributes(() -> Ipopt.Optimizer(), "print_level" => 1)
+
+# Load Data
+# ---------
+# Load the MATPOWER data file
+data = PowerModels.parse_file("Final_Grid/updated models/cal_grid_acpf_v4.m")
+
+###########
+## Solve ##
+###########
+
+solution = PowerModels.solve_opf(data, DCPPowerModel, solver)
+
+# Save solution dictionary to JSON
+if save_to_JSON == true
+    stringdata = JSON.json(solution)
+    
+    # write the file with the stringdata variable information
+    open("pf_solution.json", "w") do f
+        write(f, stringdata)
+    end
+end
